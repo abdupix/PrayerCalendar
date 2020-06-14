553185aa583065cbc9c4f59daef92b09e57130d9
CoolestDude420 <47911637+CoolestDude420@users.noreply.github.com>
2020-06-15 11:47:02 +1200

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -1,13 +1,26 @@
 // variable ste
 var checkbox = document.querySelector("input[name=theme]");
+localStorage.setItem("data-theme", "light");
 // if the switch is checkd the dark theme attritube will change to dark and display a warnig
 checkbox.addEventListener("change", function() {
 	if (this.checked) {
 		trans()
 		document.documentElement.setAttribute("data-theme", "dark")
+		if (typeof(Storage) !== "undefined") {
+			// Store
+			localStorage.setItem("data-theme", "dark");
+			// Retrieve
+			console.log(localStorage.getItem("data-theme") + " retrieve");
+		  }
 	} else {
 		trans()
 		document.documentElement.setAttribute("data-theme", "light")
+		if (typeof(Storage) !== "undefined") {
+			// Store
+			localStorage.setItem("data-theme", "light");
+			// Retrieve
+			console.log(localStorage.getItem("data-theme") + " retrive");
+		  }
 	}
 })
 // transition for switching to night mode
@@ -17,3 +30,13 @@ let trans = () => {
 		document.documentElement.classList.remove("transition")
 	}, 1000)
 }
+
+if(localStorage.getItem("data-theme") == "light"){
+	document.documentElement.setAttribute("data-theme", "light");
+	console.log("light mode scum");
+}
+if(localStorage.getItem("data-theme") == "dark"){
+	document.documentElement.setAttribute("data-theme", "dark");
+	console.log("night mode hero");
+}
+
