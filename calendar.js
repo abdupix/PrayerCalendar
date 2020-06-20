4d1e30cd1579458a75493d1f3dacc0809bb287c3
mfadam786 <mfadam786@gmail.com>
2020-06-20 18:16:08 +1200

    Changed API get method to get information for entire calendar month

--- calendar.js
+++ calendar.js
@@ -1,65 +1,102 @@
 //Gets the element on the page being used to display the api data
 const output = document.getElementById('test')
-// Function used to get the curent system date
-var currentDate = function(separator) {
-	//Creates a new instance of today's date
-	today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth() + 1; //Adds 1 to the month because it is 0 based
-	var yyyy = today.getFullYear();
-	//Appends a 0 to the front of the date or month variable if the value is less than 10
-	if (dd < 10) dd = '0' + dd;
-	if (mm < 10) mm = '0' + mm;
-	return (dd + separator + mm + separator + yyyy);
-};
+
+//Creates a new instance of today's date
+today = new Date();
+
+var dd = today.getDate();
+var mm = today.getMonth()+1; //Adds 1 to the month because it is 0 based
+var yyyy = today.getFullYear();
+
+var hh = today.getHours();
+var min = today.getMinutes();
+var currentTime = hh + ":" + min;
+
+//Appends a 0 to the front of the date or month variable if the value is less than 10
+if(dd < 10) 
+    dd='0' + dd;
+
+if(mm < 10) 
+    mm='0' + mm;
+
 //Variables for easier referencing
-let api = 'https://api.aladhan.com/v1/timings/'
-let date = currentDate('-')
+let api = 'http://api.aladhan.com/v1/calendar'
 let latitude = '-45.87416'
 let longitude = '170.50361'
 let method = 2
+
 //Fetch used to query the API for the prayer times
-fetch(`${api}${date}?latitude=${latitude}&longitude=${longitude}&method=${method}`).then(res => res.json()).then(d => {
+fetch(`${api}?latitude=${latitude}&longitude=${longitude}&method=${method}&month=${mm}&year=${yyyy}`)
+.then(res => res.json())
+.then(d => {
 	//Checks if the request was successful and the page exists
 	if (d.code == 200) {
 		//Prints the entire raw query to the console for easy debugging
 		console.log(d)
+
+		var prayerTimes = []
+        var dates = []
+
+        for (let i = 0; i < d.data.length; i++) {
+            prayerTimes[i] = d.data[i].timings
+            dates[i] = d.data[i].date.gregorian.date
+        }
+
+        let FajrTimeToday = prayerTimes[dd-1].Fajr
+        let DhuhrTimeToday = prayerTimes[dd-1].Dhuhr
+        let AsrTimeToday = prayerTimes[dd-1].Asr
+        let MaghribTimeToday = prayerTimes[dd-1].Maghrib
+        let IshaTimeToday = prayerTimes[dd-1].Isha
+        
+        let FajrTimeTomorrow = prayerTimes[dd].Fajr
+        let DhuhrTimeTomorrow = prayerTimes[dd].Dhuhr
+        let AsrTimeTomorrow = prayerTimes[dd].Asr
+        let MaghribTimeTomorrow = prayerTimes[dd].Maghrib
+		let IshaTimeTomorrow = prayerTimes[dd].Isha  
+		
 		//Appends the fetched data to an element on the page
-		document.getElementById("fajrTime").innerHTML = `${d.data.timings.Fajr}` + " AM"
-		document.getElementById("dhuhrTime").innerHTML = `${d.data.timings.Dhuhr}` + " PM"
-		document.getElementById("asrTime").innerHTML = `${d.data.timings.Asr}` + " PM"
-		document.getElementById("maghribTime").innerHTML = `${d.data.timings.Maghrib}` + " PM"
-		document.getElementById("ishaTime").innerHTML = `${d.data.timings.Isha}` + " PM"
+		if (currentTime > IshaTimeToday || currentTime < FajrTimeTomorrow || currentTime == FajrTimeTomorrow) {
+			document.getElementById("fajrTime").innerHTML = `${FajrTimeToday}` + " AM"
+			document.getElementById("dhuhrTime").innerHTML = `${DhuhrTimeToday}` + " PM"
+			document.getElementById("asrTime").innerHTML = `${AsrTimeToday}` + " PM"
+			document.getElementById("maghribTime").innerHTML = `${MaghribTimeToday}` + " PM"
+			document.getElementById("ishaTime").innerHTML = `${IshaTimeToday}` + " PM"
+		}
+		else {
+			document.getElementById("fajrTime").innerHTML = `${FajrTimeTomorrow}` + " AM"
+			document.getElementById("dhuhrTime").innerHTML = `${DhuhrTimeTomorrow}` + " PM"
+			document.getElementById("asrTime").innerHTML = `${AsrTimeTomorrow}` + " PM"
+			document.getElementById("maghribTime").innerHTML = `${MaghribTimeTomorrow}` + " PM"
+			document.getElementById("ishaTime").innerHTML = `${IshaTimeTomorrow}` + " PM"
+		}
+		// document.getElementById("fajrTime").innerHTML = `${d.data.timings.Fajr}` + " AM"
+		// document.getElementById("dhuhrTime").innerHTML = `${d.data.timings.Dhuhr}` + " PM"
+		// document.getElementById("asrTime").innerHTML = `${d.data.timings.Asr}` + " PM"
+		// document.getElementById("maghribTime").innerHTML = `${d.data.timings.Maghrib}` + " PM"
+		// document.getElementById("ishaTime").innerHTML = `${d.data.timings.Isha}` + " PM"
 	}
 	//Prints an error message to the console if the request was unsuccessful or the page doesn't exist
 	else if (d.code == 404) {
 		console.log("Error! There is a problem with the API. Server returned a status of 404 page not found. PLease reload the page")
 	}
-	var hh = today.getHours();
-	var min = today.getMinutes();
-	var currentTime = hh + ":" + min;
-	var FajrTime = `${d.data.timings.Fajr}`;
-	var DhuhrTime = `${d.data.timings.Dhuhr}`;
-	var AsrTime = `${d.data.timings.Asr}`;
-	var MaghribTime = `${d.data.timings.Maghrib}`;
-	var IshaTime = `${d.data.timings.Isha}`;
-	if (currentTime > FajrTime & currentTime < DhuhrTime || currentTime == DhuhrTime) {
+	
+	if (currentTime > FajrTimeToday & currentTime < DhuhrTimeToday || currentTime == DhuhrTimeToday) {
 		document.getElementById("DhuhrPrayer").classList.add("nextPrayer");
 	}
-	if (currentTime > DhuhrTime & currentTime < AsrTime || currentTime == AsrTime) {
+	if (currentTime > DhuhrTimeToday & currentTime < AsrTimeToday || currentTime == AsrTimeToday) {
 		document.getElementById("AsrPrayer").classList.add("nextPrayer");
-		var nekPrayer = Math.abs(parseInt(currentTime) - parseInt(AsrTime));
-		console.log(parseInt(currentTime));
-		console.log(AsrTime);
-		console.log(nekPrayer);
+		// var nekPrayer = Math.abs(parseInt(currentTime) - parseInt(AsrTime));
+		// console.log(parseInt(currentTime));
+		// console.log(AsrTime);
+		// console.log(nekPrayer);
 	}
-	if (currentTime > AsrTime & currentTime < MaghribTime || currentTime == MaghribTime) {
+	if (currentTime > AsrTimeToday & currentTime < MaghribTimeToday || currentTime == MaghribTimeToday) {
 		document.getElementById("MaghribPrayer").classList.add("nextPrayer");
 	}
-	if (currentTime > MaghribTime & currentTime < IshaTime || currentTime == IshaTime) {
+	if (currentTime > MaghribTimeToday & currentTime < IshaTimeToday || currentTime == IshaTimeToday) {
 		document.getElementById("IshaPrayer").classList.add("nextPrayer");
 	}
-	if (currentTime > IshaTime || currentTime < FajrTime || currentTime == FajrTime) {
+	if (currentTime > IshaTimeToday || currentTime < FajrTimeTomorrow || currentTime == FajrTimeTomorrow) {
 		document.getElementById("FajrPrayer").classList.add("nextPrayer");
 	}
 })
